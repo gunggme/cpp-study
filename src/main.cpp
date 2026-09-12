e3522ec8b92820a14e2714cc9eb8e396726cd84d
gunggme <ekfrifajfl2019@gmail.com>
2026-09-12 15:11:07 +0900

    omg... this is heap initialgit add .!

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,21 @@
 #include<iostream>
 
 int main(){
-    // 오리지널 변수 생성
-    cppstudy::Vec2 original{10.0f, 20.0f};
+    // 1. stack에 생성
+    cppstudy::Vec2 stackVec{10.0f, 20.0f};
 
-    // 그냥 오리지널 변수값을 가진 변수 초기화
-    cppstudy::Vec2 value = original;
-    // 오리지널 변수값의 주소값을 가진 변수 초기화
-    cppstudy::Vec2& ref = original;
-    // 오리지널 변수값의 주소값으로 초기화
-    cppstudy::Vec2* ptr = &original;
+    // 2. heap에 생성
+    cppstudy::Vec2* heapVec = new cppstudy::Vec2{30.0f, 40.0f};
 
-    ptr->y = 999.0f;
-
-    // 오리지널 주소 출력
-    std::cout << "original address  : " << &original << '\n';
-    // 일반 변수 주소 출력
-    std::cout << "value adress      : " << &value << "\n";
-    // 주소값 출력
-    std::cout << "ref adddress      : " << &ref << "\n";
-    // 주소값으로 초기화된 변수 출력
-    std::cout << "ptr value         : " << ptr << "\n";
-
-    std::cout << '\n';
+    std::cout << "stackVec address      : " << &stackVec << "\n";
     
-    std::cout << "original  : " << original.x << ", " << original.y << "\n";
-    std::cout << "value     : " << value.x << ", " << value.y << "\n";
-    std::cout << "ref       : " << ref.x << ", " << ref.y << "\n";
-    std::cout << "ptr       : " << ptr->x << ", " << ptr->y << "\n";
+    std::cout << "heapVec ptr address   : " << &heapVec << "\n";
+    std::cout << "heapVec value         : " << heapVec << "\n";
+
+    std::cout << "heapVec x             : " << heapVec->x << "\n";
+    std::cout << "heapVec y             : " << heapVec->y << "\n";
+
+    delete heapVec;
 
     return 0;
 }
