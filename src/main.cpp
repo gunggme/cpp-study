2c5a26819ddace1256751b08b0596d743c76d17f
gunggme <ekfrifajfl2019@gmail.com>
2026-09-12 14:52:33 +0900

    oh my godgit add .! is trash funtiongit add .!

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,37 @@
 
 #include<iostream>
 
+cppstudy::Vec2& makeVec();
+
 int main(){
-    cppstudy::Vec2 a{3.0f, 4.0f};
-    cppstudy::Vec2 b{1.0f, 2.0f};
+    // cppstudy::Vec2 a{3.0f, 4.0f};
+    // cppstudy::Vec2 b{1.0f, 2.0f};
 
-    const auto c = a + b;
+    // const auto c = a + b;
 
-    std::cout << "a.length() : " << a.length() << '\n';
-    std::cout << "c: " << c.x << ", " << c.y << '\n';
+    // std::cout << "a.length() : " << a.length() << '\n';
+    // std::cout << "c: " << c.x << ", " << c.y << '\n';
+
+    cppstudy::Vec2& vec = makeVec();
+
+    std::cout << "vec address : " << &vec << "\n";
+    std::cout << "vec.x : " << &vec.x << ", vec.y : " << &vec.y << "\n";
+    std::cout << "vec.x : " << vec.x << ", vec.y : " << vec.y << "\n";
+
+    std::cout << "sizeof(float): "
+          << sizeof(float)
+          << '\n';
+
+    std::cout << "sizeof(Vec2): "
+          << sizeof(cppstudy::Vec2)
+          << '\n';
 
     return 0;
+}
+
+cppstudy::Vec2& makeVec()
+{
+    cppstudy::Vec2 v{1.0f, 2.0f};
+
+    return v;
 }
